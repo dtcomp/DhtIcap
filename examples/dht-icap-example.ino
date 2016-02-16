fabddaf7da82f6334ef3c5843cfb22254cdd83d0
Mike Figley <mike@dtcomp.info>
2016-02-16 05:17:56 -0900

    Added the actual example code...

--- examples/dht-icap-example.ino
+++ examples/dht-icap-example.ino
@@ -1,46 +1,44 @@
-//#define DHT_ICAP_DEBUG
-#include <SoftwareSerial.h>
 #include <DhtIcap.h>
 
-#define DHT_DATA PB0
+#define DHT_DATA PB0   // PIN 8 on Uno
 #define DHT_POWER A0
 #define DHT_TYPE 22
 
 void myErrCallback(const uint8_t status) {
-  Serial.print("DHT Error: ");
+  Console.print("DHT Status: ");
   if ( !status )
-    Serial.println("OK");
+    Console.println("OK");
   if ( status & DHT_ICAP_ERR_SUM )
-    Serial.println("Checksum Error");
+    Console.println("Checksum Error");
   if ( status & DHT_ICAP_ERR_CNT )
-    Serial.println("Too few bits from sensor (timeout)");
+    Console.println("Too few bits from sensor (timeout)");
   if ( status & DHT_ICAP_ERR_TONE )
-    Serial.println("Edges polytonic");
+    Console.println("Edges polytonic");
   if ( status & DHT_ICAP_ERR_LINE )
-    Serial.print("Unresolved bit");
+    Console.print("Unresolved bit");
   if ( status & DHT_ICAP_ERR_CAL )
-    Serial.println("Data timing calibtation failed");
+    Console.println("Data timing calibtation failed");
   if ( status & DHT_ICAP_ERR_OVR )
-    Serial.println("Data overflow (too many bits)");
+    Console.println("Data overflow (too many bits)");
   if ( status & DHT_ICAP_ERR_INIT )
-    Serial.println("DhtIcap is uninitialized - run <YourDhtIcapVar>.begin()?");
+    Console.println("DhtIcap is uninitialized - run <YourDhtIcapVar>.begin()?");
 }
 
 // Instantiate
-DhtIcap dht(DHT_TYPE, 8, myErrCallback);
-SoftwareSerial Console(10,11);
+DhtIcap dht(DHT_TYPE, myErrCallback, 8);
+
 void setup() {
-  Console.begin(9600);
-  Console.println("Gotta Console!!!");
   // put your setup code here, to run once:
   Serial.begin(115200);
+  while (Serial.available())
+    Serial.read();
   while (!Serial.available()) {
-    Serial.println("Press any key to start.");
+    Console.println("Press any key to start.");
     delay (1000);
   }
-  Serial.println("DhtIcap Example sketch");
-  Serial.print("Version: ");
-  Serial.println(DHT_ICAP_VERSION);
+  Console.println("DhtIcap Example sketch");
+  Console.print("Version: ");
+  Console.println(DHT_ICAP_VERSION);
   pinMode(DHT_DATA, OUTPUT);
   digitalWrite(DHT_DATA, LOW);
   pinMode(DHT_POWER, OUTPUT);
@@ -48,37 +46,59 @@ void setup() {
   digitalWrite(DHT_POWER, HIGH);
   delay(200);
   digitalWrite(DHT_DATA, HIGH);
-  dht.begin();
-  if (!dht.valid()) {
-    Serial.println("DHT not working, try #define DHT_ICAP_VERB");
-    Serial.println("and/or #define DHT_ICAP_DEBUG");
-    Serial.println("then, dht.statusPrint();");
+  if (!dht.begin()) {
+    Console.println("DHT not working");
+  } else {
+    Console.println("Initialized.");
   }
 }
 
-float f, c, k, h;
-double d, D;
 
 void loop() {
+  Serial.print("Main Loop");
+  delay(2200);
 
-  f = dht.fahrenheit();
-  h = dht.humidity();
-  c = dht.celsius();
-  k = dht.kelvin();
-  d = dht.dewPoint();
-  D = dht.dewPointSlow();
+  DhtIcap::Data data;
 
-  Serial.print("T = ");
-  Serial.print(f); Serial.print("F : ");
-  Serial.print(c); Serial.print("C : ");
-  Serial.print(k); Serial.println('K');
-  Serial.print("H = ");
-  Serial.print(h); Serial.println('%');
-  Serial.print("d = "); Serial.print(d); Serial.print("C : ");
-  Serial.print(d * 9.0 / 5.0 + 32.0); Serial.println('F');
-  Serial.print("D = "); Serial.print(d); Serial.print("C : ");
-  Serial.print(D * 9.0 / 5.0 + 32.0); Serial.println("F\n");
-  delay(2000);
+  if (dht.read(&data, 'F', 'C')) {
+    // do the send
+    Console.print("T = ");
+    Console.print(data.temperature.val);
+    Console.println(data.temperature.unit);
+    Console.print("H = ");
+    Console.print(data.humidity);
+    Console.println('%');
+    Console.print("D = ");
+    Console.print(data.dewpoint.val);
+    Console.println(data.dewpoint.unit);
+  } else {
+    Console.println("Error!");
+    myErrCallback(dht.status());
+    // there was an error...
+  }
+
+
+// Or, use the discrete functions
+//
+//  float f, c, k, h;
+//  double d, D;
+//  f = dht.fahrenheit();
+//  h = dht.humidity();
+//  c = dht.celsius();
+//  k = dht.kelvin();
+//  d = dht.dewPoint();
+//  D = dht.dewPoint();
+  //  Console.print("T = ");
+  //  Console.print(f); Console.print("F : ");
+  //  Console.print(c); Console.print("C : ");
+  //  Console.print(k); Console.println('K');
+  //  Console.print("H = ");
+  //  Console.print(h); Console.println('%');
+  //  Console.print("d = "); Console.print(d); Console.print("C : ");
+  //  Console.print(d * 9.0 / 5.0 + 32.0); Console.println('F');
+  //  Console.print("D = "); Console.print(d); Console.print("C : ");
+  //  Console.print(D * 9.0 / 5.0 + 32.0); Console.println("F\n");
+
 }
 
 
